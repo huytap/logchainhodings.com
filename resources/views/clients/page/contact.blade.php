88c14668b660f7f5d99c9aa161571e6cba7fa551
Nguyễn Huy Tập <nghuytap@gmail.com>
2024-01-07 08:44:59 +0700

    contact

--- resources/views/clients/page/contact.blade.php
+++ resources/views/clients/page/contact.blade.php
@@ -15,31 +15,40 @@ $class='contact__banner';
             <h1 class="banner__title2 wow fadeInUp" data-wow-duration="1.5s">
                 @lang('logchain.title.contact')
             </h1>
-        </div> 
-    </div>        
+        </div>
+    </div>
 </div>
-<div class="content content__contact">        
+<div class="content content__contact">
     <div class="contact">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-2 offset-md-1 contact__item wow fadeInUp" data-wow-duration="1.5s">
-                    {{-- <h3>{{ strip_tags(Setting::getValue('owner'))}}</h3>
-                    <div class="contact__us">
-                        <p>{{Setting::getValue('job_title')}}</p>
-                        <a href="mailto:{{Setting::getValue('contact_email')}}">{{Setting::getValue('contact_email')}}</a>
-                        <a href="tel:{{Setting::getValue('contact_phone')}}">{{Setting::getValue('contact_phone')}}</a>
-                    </div> --}}
-                    <div class="contact__branch">
-                        <h4>{!! $title_1[$lang] !!}</h4>
-                        <p>{!! $add_1[$lang] !!}</p>
-                        <a href="tel:{!!Setting::getValue('contact_phone')!!}">{!!Setting::getValue('contact_phone')!!}</a>
+            <div class="row align-items-center">
+                <div class="col-md-4 offset-md-1">
+                    <div class="row">
+                        <div class="col-md-6 wow fadeInUp" data-wow-duration="1.5s">
+                            {{-- <h3>{{ strip_tags(Setting::getValue('owner'))}}</h3>
+                            <div class="contact__us">
+                                <p>{{Setting::getValue('job_title')}}</p>
+                                <a href="mailto:{{Setting::getValue('contact_email')}}">{{Setting::getValue('contact_email')}}</a>
+                                <a href="tel:{{Setting::getValue('contact_phone')}}">{{Setting::getValue('contact_phone')}}</a>
+                            </div> --}}
+                            <div class="contact__branch">
+                                <h4>{!! $title_1[$lang] !!}</h4>
+                                <p>{!! $add_1[$lang] !!}</p>
+                                <a href="tel:{!!Setting::getValue('contact_phone')!!}">{!!Setting::getValue('contact_phone')!!}</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 wow fadeInUp" data-wow-duration="1.5s">
+                            <div class="contact__branch">
+                                <h4>{!! $title_2[$lang] !!}</h4>
+                                <p>{!! $add_2[$lang] !!}</p>
+                                <a href="tel:{!!Setting::getValue('contact_phone_2')!!}">{!!Setting::getValue('contact_phone_2')!!}</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-2 contact__item wow fadeInUp" data-wow-duration="1.5s">
-                    <div class="contact__branch">
-                        <h4>{!! $title_2[$lang] !!}</h4>
-                        <p>{!! $add_2[$lang] !!}</p>
-                        <a href="tel:{!!Setting::getValue('contact_phone_2')!!}">{!!Setting::getValue('contact_phone_2')!!}</a>
+                    <div class="row mt-3">
+                        <div class="col-sm-12">
+                            <strong>Email</strong> <a style="font-weight: normal;" href="mailto:{{Setting::getValue('contact_email')}}">{{Setting::getValue('contact_email')}}</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-6 offset-md-1">
@@ -54,10 +63,10 @@ $class='contact__banner';
 @endsection
 @section('script')
 <script type="text/javascript">
-// setTimeout(function(){
-//     var bannerHeight = document.getElementById('banner')
-//     var height = bannerHeight.offsetHeight - 100
-//     document.getElementById('banner').style.height = height + 'px'
-// }, 1000)
+    // setTimeout(function(){
+    //     var bannerHeight = document.getElementById('banner')
+    //     var height = bannerHeight.offsetHeight - 100
+    //     document.getElementById('banner').style.height = height + 'px'
+    // }, 1000)
 </script>
 @endsection
